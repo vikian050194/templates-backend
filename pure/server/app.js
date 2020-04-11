c7c85522d1a815015488e8876c11d582f233baf9
Kirill Vinogradov <vikian050194@gmail.com>
2020-04-11 18:55:51 +0300

    Add simple http server implementation to pure template

--- pure/server/app.js
+++ pure/server/app.js
@@ -1,1 +1,19 @@
-console.info("Hello, World!");
+const http = require("http");
+const url = require("url");
+const port = process.env.PORT || 8080;
+
+http.createServer(function (req, res) {
+    res.writeHead(200, { "Content-Type": "application/json" });
+
+    const { query } = url.parse(req.url, true);
+    const { id } = query;
+
+    const result = {
+        next: parseInt(id) + 1
+    };
+
+    res.write(JSON.stringify(result));
+    res.end();
+}).listen(port);
+
+console.info(`Server is listening on port ${port}`);
