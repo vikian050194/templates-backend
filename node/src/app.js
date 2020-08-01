2f49d6e805a256a1004f2418232db07e7fc1a767
Kirill Vinogradov <vikian050194@gmail.com>
2020-08-01 09:56:35 +0300

    Add logger to node template

--- node/src/app.js
+++ node/src/app.js
@@ -1,22 +1,35 @@
-const http = require("http");
-const url = require("url");
 const port = process.env.PORT || 8081;
 
+const http = require("http");
+const url = require("url");
+const morgan = require("morgan");
+// const finalhandler = require("finalhandler");
+
+const loggerOutputPredefinedFormat = "dev";
+const logger = morgan(loggerOutputPredefinedFormat);
+
 http.createServer(function (req, res) {
-    res.writeHead(200, { "Content-Type": "application/json" });
+    // var done = finalhandler(req, res);
 
-    const { query } = url.parse(req.url, true);
-    const { id } = query;
-    const nextId = parseInt(id) + 1;
+    // eslint-disable-next-line no-unused-vars
+    logger(req, res, function (err) {
+        //   if (err) return done(err)
 
-    console.info(nextId);
+        res.writeHead(200, { "Content-Type": "application/json" });
 
-    const result = {
-        nextId
-    };
+        const { query } = url.parse(req.url, true);
+        const { id } = query;
+        const nextId = parseInt(id) + 1;
 
-    res.write(JSON.stringify(result));
-    res.end();
+        console.info(nextId);
+
+        const result = {
+            nextId
+        };
+
+        res.write(JSON.stringify(result));
+        res.end();
+    });
 }).listen(port);
 
 console.info(`Server is listening on port ${port}`);
