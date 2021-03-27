c46b6714a971d1808f69fd7ed83f0c59fffac4e3
Kirill Vinogradov <vikian050194@gmail.com>
2021-03-27 09:34:22 +0300

    add node/check

--- node/src/proxy.js
+++ node/src/proxy.js
@@ -1,11 +1,11 @@
-var http = require("http");
+const http = require("http");
 
 http.createServer(onRequest).listen(3030);
 
 function onRequest(client_req, client_res) {
     console.info("serve: " + client_req.url);
 
-    var options = {
+    const options = {
         hostname: "localhost",
         port: 80,
         path: client_req.url,
@@ -13,7 +13,7 @@ function onRequest(client_req, client_res) {
         headers: client_req.headers
     };
 
-    var proxy = http.request(options, function (res) {
+    const proxy = http.request(options, function (res) {
         client_res.writeHead(res.statusCode, res.headers);
         res.pipe(client_res, {
             end: true
